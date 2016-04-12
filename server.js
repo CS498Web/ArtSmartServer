e48f3b13f8dad7f9faff46dc18a16860b0725192
Robert Cory Larson <rlarson2@illinois.edu>
2016-04-11 20:02:22 -0500

    Back End Complete

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 // Get the packages we need
 var express = require('express');
 var mongoose = require('mongoose');
-var Llama = require('./models/llama');
+//var Llama = require('./models/llama');
+var User = require('./models/user');
+var Task = require('./models/task');
 var bodyParser = require('body-parser');
 var router = express.Router();
 
 //replace this with your Mongolab URL
-mongoose.connect('mongodb://localhost/mp4');
+mongoose.connect('mongodb://rlarson2:buddy5694@ds019990.mlab.com:19990/cs498mp4');
 
 // Create our Express application
 var app = express();
@@ -38,11 +40,260 @@ homeRoute.get(function(req, res) {
 });
 
 //Llama route
-var llamaRoute = router.route('/llamas');
+//var llamaRoute = router.route('/llamas');
+var usersRoute = router.route('/users');
+var tasksRoute = router.route('/tasks');
+var userRoute = router.route('/users/:id');
+var taskRoute = router.route('/tasks/:id');
 
-llamaRoute.get(function(req, res) {
-  res.json([{ "name": "alice", "height": 12 }, { "name": "jane", "height": 13 }]);
+usersRoute.get(function(req, res) {  //NEED TO ADD THE SPECIFIC STUFF  /api/users?where = {}&sort=___&cont
+	var query = User.find(); 
+	var whereQuery = req.query["where"];
+	var sortQuery = req.query["sort"];
+	var selectQuery = req.query["select"];
+	var skipQuery = req.query["skip"];
+	var limitQuery = req.query["limit"];
+	var countQuery = req.query["count"];
+	 console.log(whereQuery);
+	 if(whereQuery==undefined) {
+	 	whereQuery="{}";
+	 }
+	 var wherejson = JSON.parse(whereQuery);
+	 query.where(wherejson);
+	 if(sortQuery) {
+	 	query.sort(JSON.parse(sortQuery));
+	 }
+	if(selectQuery) {
+		query.select(JSON.parse(selectQuery));
+	}
+	if(skipQuery && !isNaN(parseInt(skipQuery))) {
+		query.skip(skipQuery);
+	}
+	if(limitQuery && !isNaN(parseInt(limitQuery))) {
+		query.limit(limitQuery);
+	}
+	if(Boolean(countQuery)){
+		query.count();
+	}
+        query.exec(function (err, users) {
+                if (err)
+                    res.status(500).json({ message: 'Could not complete request', data: [] });
+
+                else
+                    res.status(200).json({ message: 'Completed Request', data: users });
+            })
 });
+
+usersRoute.post(function(req, res) {  //done
+	var user = new User();
+
+	user.name = req.body.name;
+	user.email = req.body.email;
+	user.pendingTasks = req.body.pendingTasks;
+	user.dateCreated = Date.now();
+
+	user.save(function(err) {
+		if(err){
+			res.status(500);
+			res.json( { message: 'Failed to create user', data: [] } );
+		}
+		else {
+			res.status(201);
+			res.json( { message: 'Created User', data: user } );
+		}
+	});
+});
+
+usersRoute.options(function(req, res){  //done
+      res.writeHead(200);
+      res.end();
+});
+
+userRoute.get(function(req, res) {  //done
+	User.findById(req.params.id, function(err, user){
+        if(err){ 
+            res.status(404);
+            res.json( { message: 'User not found', data: [] } );
+        }
+        else {
+            res.status(200);
+            res.json({ message: 'Found User', data: user } );
+        }
+	});
+});
+
+userRoute.put(function(req, res) {  //done
+	User.findById(req.params.id, function(err, user){
+		if(err){
+			res.status(404);
+			res.json( { message: 'User not found', data: [] } );
+		}
+
+		if(!req.body.pendingTasks)
+            user.pendingTasks = [];
+        else 
+            user.pendingTasks = req.body.pendingTasks;
+
+        user.save(function(err, user) {
+            if (err){
+                res.status(500);
+            	res.json({ message: 'Failed to save user', data: [] });
+            }
+            else{
+                res.status(200);
+                res.json({ message: 'Updated User', data: user } );
+            }
+        });
+	});
+});
+
+userRoute.delete(function(req, res) {  //done
+	User.findById(req.params.id, function(err, user){
+		if(err){
+			res.status(404);
+			res.json( { message: 'User not found to delete', data: [] } );
+		}
+		else{
+			User.findByIdAndRemove(req.params.id, function(err) {
+				if(err){
+					res.status(404);
+					res.json( {message: 'Failed to delete user', data: [] } );
+				}
+				else {
+					res.status(200);
+					res.json( {message: 'Deleted User', data: [] } );
+				}
+			});
+		}
+	});
+});
+
+tasksRoute.get(function(req, res) {  //NEED TO ADD THE SPECIFIC STUFF
+	var query = Task.find(); 
+	var whereQuery = req.query["where"];
+	var sortQuery = req.query["sort"];
+	var selectQuery = req.query["select"];
+	var skipQuery = req.query["skip"];
+	var limitQuery = req.query["limit"];
+	var countQuery = req.query["count"];
+	 console.log(whereQuery);
+	 if(whereQuery==undefined) {
+	 	whereQuery="{}";
+	 }
+	 var wherejson = JSON.parse(whereQuery);
+	 query.where(wherejson);
+	 if(sortQuery) {
+	 	query.sort(JSON.parse(sortQuery));
+	 }
+	if(selectQuery) {
+		query.select(JSON.parse(selectQuery));
+	}
+	if(skipQuery && !isNaN(parseInt(skipQuery))) {
+		query.skip(skipQuery);
+	}
+	if(limitQuery && !isNaN(parseInt(limitQuery))) {
+		query.limit(limitQuery);
+	}
+	else {
+		query.limit(100)
+	}
+	if(Boolean(countQuery)){
+		query.count();
+	}
+        query.exec(function (err, users) {
+                if (err)
+                    res.status(500).json({ message: 'Could not complete request', data: [] });
+
+                else
+                    res.status(200).json({ message: 'Completed Request', data: users });
+            })
+});
+
+tasksRoute.post(function(req, res) { //done
+	var task = new Task();
+
+    task.name = req.body.name;
+    task.description = (req.body.description ? req.body.description : "");
+    task.deadline = req.body.deadline;
+    task.completed = (req.body.completed ? req.body.completed : false);
+    task.assignedUser = (req.body.assignedUser ? req.body.assignedUser : "");
+    task.assignedUserName = (req.body.assignedUserName ? req.body.assignedUserName : "unassigned");
+    task.dateCreated = Date.now();
+
+
+    task.save(function(err) {
+        if (err){
+            res.status(500);
+        	res.json({ message: 'Could not add task', data: [] });
+        }
+        else
+            res.status(201);
+        	res.json({ message: 'Added to task', data: task });
+    });
+});
+
+tasksRoute.options(function(req, res){  //done
+      res.writeHead(200);
+      res.end();
+});
+
+taskRoute.get(function(req, res) { //done
+    Task.findById(req.params.id, function(err, task){
+        if(err)
+            res.status(404).json({ message: 'Unable tocomplete request', data: [] });
+
+        else
+            res.status(200).json({ message: 'Completed Request', data: task });
+    });
+});
+
+taskRoute.put(function(req, res) {  //done
+
+    Task.findById(req.params.id, function(err, task) {
+
+        if (err){
+            res.status(404);
+            res.json({ message: 'Unable to complete request', data: err });
+        }
+
+        task.name= req.body.name;
+        task.description= req.body.description || "";
+        task.deadline= req.body.deadline;
+        task.completed= req.body.completed  || false;
+        task.assignedUser= req.body.assignedUser  || "";
+        task.assignedUserName= req.body.assignedUserName  || "unassigned";
+
+
+        task.save(function(err) {
+            if (err){
+                res.status(500);
+                res.json({ message: 'Could not update task', data: err });
+            }
+            else{
+                res.status(200);
+                res.json({ message: 'Updated task', data: task });
+            }
+        });
+
+    });
+});
+
+taskRoute.delete(function(req, res) {
+    Task.findByIdAndRemove(req.params.id, function(err) {
+        if (err){
+            res.status(404);
+            res.json({ message: 'Unable to delete task', data: err });
+        }
+
+        else{
+            res.status(200);
+        	res.json({ message: 'Task deleted', data:{} });
+        }
+    });
+});
+// llamaRoute.get(function(req, res) {
+//   res.json([{ "name": "alice", "height": 12 }, { "name": "jane", "height": 13 }]);
+// });
 
 //Add more routes here
 
