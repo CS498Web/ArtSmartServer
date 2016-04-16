00e5bb561760e6e76e1c4aa056a2204db65ac6ba
nathan <nathankou12@gmail.com>
2016-04-16 20:16:38 +0000

    adf

---
+++ models/album.js
@@ -0,0 +1,57 @@
+var mongoose = require('mongoose');
+
+var AlbumSchema = new mongoose.Schema({
+	title: {
+		type: String
+	},
+	artists: [{
+		type: String
+	}],
+	location: {
+		type: String
+	},
+	period: {
+		type: String
+	},
+	year: {
+		type: String
+	},
+	media: {
+		type: String
+	},
+	size: [{
+		type: String
+	}],
+	src: {
+		type: String
+	},
+	annotations: [
+		{
+			shape: {
+				type: String
+			},
+			x: {
+				type: Number
+			},
+			y: {
+				type: Number
+			},
+			radius: {
+				type: Number
+			},
+			text: {
+				type: String
+			},
+			comments: [{
+				user: {
+					type: String
+				},
+				text: {
+					type: String
+				}
+			}]
+		}
+	]
+});
+
+module.exports = mongoose.model('Album', AlbumSchema);
