c13105464ebf18aca7cd0e9b8d1f9bd52edceb76
Robert Cory Larson <rlarson2@illinois.edu>
2016-05-01 18:57:55 -0500

    we login

--- server.js
+++ server.js
@@ -37,6 +37,7 @@ var allowCrossDomain = function(req, res, next) {
   res.header('Access-Control-Allow-Credentials', true);
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH');
   next();
+  
 };
 app.use(allowCrossDomain);
 app.use(passport.initialize());
